34d4fbdef40a22db45bc164593ba73258987cdc7
Victor Randall <victor.randall13@gmail.com>
2021-07-08 20:54:59 -0300

    changes on custom node

--- src/KinematicBody2D.gd
+++ src/KinematicBody2D.gd
@@ -5,29 +5,22 @@ var gravity:float = 80
 var speed:float = 110
 var jump_height:float = 130
 
-var anim_state:int
-var anim_attack:int
-var anim_moviment:int
-var transition:bool = false
-var anim_finished:bool = false
-var is_falling:bool = false
-enum States{
+enum states{
 	none
+	
+	turning
+	crouching
 	
 	idle
 	running
 	jumping
 	jumping_moving
-	stop
-	
-	turning
-	crouching
-	dead
 	
 	shooting
 	stabing
 	throw_bomb
 }
+
 var dir:Vector2
 
 onready var anim:Node = get_node("AnimationPlayer")
@@ -40,7 +33,7 @@ func _ready():
 
 func _process(delta):
 	#debug
-	var text:String = "motion = " + str(motion) + "\nposition = " + str(position) + "\nanim_state = " + str(anim_state) + "\nanim_attack = " + str(anim_attack) + "\nanim_moviment = " + str(anim_moviment) + "\ntransition = " + str(transition) + "\nanim finished = " + str(anim_finished) + "\nis falling = " + str(is_falling) + "\ndir = " + str(dir) + "\ncurrent anim playing (torso) = " + str(anim.current_animation) + "\ncurrent anim playing (legs) = " + str(anim2.current_animation)
+	var text:String = "motion = " + str(motion) + "\nposition = " + str(position) + "\ndir = " + str(dir) + "\ncurrent anim playing (torso) = " + str(anim.current_animation) + "\ncurrent anim playing (legs) = " + str(anim2.current_animation)
 	get_parent().get_node("UI/Control/Label").text = text
 	
 	#moviment and attack
@@ -58,83 +51,20 @@ func _process(delta):
 				dir = Vector2.UP
 			elif Input.is_action_pressed("ui_down"):
 				dir = Vector2.DOWN
-			anim_moviment = running
+			$BetterAnim.anim_moviment = states.running
 			sprite.scale.x = motion_dir
 			sprite.position.x = 2.5 * motion_dir + motion_dir
 		else:
-			anim_moviment = idle
-			
+			$BetterAnim.anim_moviment = states.idle
 	else:
 		motion.y += gravity * delta# * delta
 		if motion.x == 0:
-			anim_moviment = jumping
+			$BetterAnim.anim_moviment = states.jumping
 		else:
-			anim_moviment = jumping_moving
+			$BetterAnim.anim_moviment = states.jumping_moving
 		if Input.is_action_pressed("ui_left"):
 			dir = Vector2.LEFT
 		elif Input.is_action_pressed("ui_right"):
 			dir = Vector2.RIGHT
 	
 	move_and_slide_with_snap(motion, Vector2(0,10) , Vector2.UP)
-	
-	
-	
-	#animations
-#	match anim_state:
-#		turning:
-#			sprite2.visible = false
-#		crouching:
-#			sprite2.visible = false
-#		none:
-#			sprite2.visible = true
-#	match anim_attack:
-#		shooting:
-#			pass
-##			anim.play(str(anim_attack))
-#		stabing:
-#			pass
-##			anim.play(str(anim_attack))
-#		throw_bomb:
-#			pass
-##			anim.play(str(anim_attack))
-#		none:
-#			pass
-#	match anim_moviment:
-#		idle:
-#			if anim_state == none or anim_attack == none:
-#				anim.play(str(anim_moviment))
-#			sprite.position.y = -23
-#			anim2.play(str(anim_moviment))
-#		running:
-#			if anim_state == none or anim_attack == none:
-#				anim.play(str(anim_moviment))
-#			sprite.position.y = -26
-##				anim.current_animation_position = anim2.current_animation_position
-#			anim2.play(str(anim_moviment))
-#		jumping:
-##			var anim_speed:float = motion.y * 0.15 * -1
-##			if anim2.current_animation_position == anim2.current_animation_position - anim2.current_animation_length:
-##			print(anim2.current_animation_position - anim2.current_animation_length)
-#			if anim_state == none or anim_attack == none:
-#				anim.play(str(anim_moviment))
-#			anim2.play(str(anim_moviment))
-#		jumping_moving:
-#			if anim_state == none or anim_attack == none:
-##				pass
-#				anim.play(str(anim_moviment))
-#			anim2.play(str(anim_moviment))
-
-
-func _animation_finished(anim_name):
-#	if anim.current_animation == 
-	anim_finished = true
-#	if anim_state != none:
-#		anim_state = none
-#	if anim_attack != none:
-#		anim_attack = none
-	
-
-
-func _animation_started(anim_name):
-	anim_finished = false
-#	print(finished)
--- src/better_anim.gd
+++ src/better_anim.gd
@@ -1,12 +1,60 @@
 extends Node
-class_name Better_Anim
+class_name BetterAnim
 
-export(Array, NodePath) var animation_players
-export(int, "String") var animations
-export(int) var current_animation
+export(Array, NodePath) var anim_nodes
 
-var anim_state:int
-var anim_attack:int
-var anim_moviment:int
+var anim_state:int = 0
+var anim_attack:int = 0
+var anim_moviment:int = 0
+
 var transition:bool = false
 var anim_finished:bool = false
+
+func _ready():
+	for i in range(anim_nodes.size()):
+#		anim_nodes[i].connect("animation_started", self, "_animation" + str(i) + "_started")
+#		anim_nodes[i].connect("animation_finished", self, "_animation" + str(i) + "_finished")
+		pass
+
+func _process(delta):
+	for i in range(anim_nodes.size()):
+		print(anim_nodes[i])
+#		var anim_name:String
+#		if anim_attack != 0:
+#			anim_name = str(anim_state) + str(anim_attack)
+#		else:
+#			anim_name = str(anim_state) + str(anim_moviment)
+#
+#
+#		if transition == true:
+#			if anim_nodes[i].has_animation(anim_name):
+#				anim_nodes[i].play(anim_name + "t")
+#		else:
+#			if anim_nodes[i].has_animation(anim_name):
+#				anim_nodes[i].play(anim_name)
+		
+
+
+func _animation1_started(anim_name):
+	pass
+
+func _animation2_started(anim_name):
+	pass
+
+func _animation3_started(anim_name):
+	pass
+
+func _animation4_started(anim_name):
+	pass
+
+func _animation1_finished(anim_name):
+	pass
+
+func _animation2_finished(anim_name):
+	pass
+
+func _animation3_finished(anim_name):
+	pass
+
+func _animation4_finished(anim_name):
+	pass
